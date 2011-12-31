7b100844bae31bd9287201cd29b5a6eb1dfa609c
ajt <github@thorntonindustries.com>
2011-12-31 10:17:48 -0500

    I have no idea what I am doing...

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+version = '.01'
+
+setup(
+    name='django-avatar',
+    version=version,
+    description="django-avatar",
+    long_description="",
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+        "Environment :: Web Environment",
+    ],
+    keywords='snapboard,forums,django',
+    author='ajt',
+    author_email='',
+    url='',
+    license='',
+    packages=find_packages(),
+    package_data = {
+        'snapboard': [
+            'templates/snapboard/*.html',
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+)
